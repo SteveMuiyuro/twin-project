7e208012ff3b36d80dc94098268902605d332e4f
SteveMuiyuro <stevemuiyuro@gmail.com>
2026-04-14 20:10:53 +0300

    Add remote terraform state backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,20 +1,20 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "gcs" {
+    bucket  = "twin-terraform-state-51053"
+    prefix  = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
-    }
   }
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project = "twin-project-51053"
+  region  = "us-central1"
 }
