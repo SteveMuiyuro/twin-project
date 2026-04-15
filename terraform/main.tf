643be9b1fd96b8966a3d97b3acdbfcf59f6925d2
SteveMuiyuro <stevemuiyuro@gmail.com>
2026-04-15 09:07:12 +0300

    Switch to Cloud Function + API Gateway architecture

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,22 @@
 ############################################
-# Random suffix for globally unique buckets
+# Enable required APIs
+############################################
+
+resource "google_project_service" "services" {
+  for_each = toset([
+    "cloudfunctions.googleapis.com",
+    "apigateway.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "storage.googleapis.com",
+    "aiplatform.googleapis.com"
+  ])
+
+  project = var.project_id
+  service = each.key
+}
+
+############################################
+# Random suffix
 ############################################
 
 resource "random_id" "suffix" {
@@ -7,184 +24,149 @@ resource "random_id" "suffix" {
 }
 
 ############################################
-# Enable required GCP APIs
+# Storage bucket (frontend)
 ############################################
 
-resource "google_project_service" "run_api" {
-  project = var.project_id
-  service = "run.googleapis.com"
-}
-
-resource "google_project_service" "artifact_registry_api" {
-  project = var.project_id
-  service = "artifactregistry.googleapis.com"
-}
-
-resource "google_project_service" "storage_api" {
-  project = var.project_id
-  service = "storage.googleapis.com"
-}
-
-############################################
-# Artifact Registry (Docker images)
-############################################
-
-resource "google_artifact_registry_repository" "backend_repo" {
-  project       = var.project_id
-  location      = var.region
-  repository_id = "${var.project_name}-${var.environment}-repo"
-  description   = "Docker repository for backend"
-  format        = "DOCKER"
-
-  depends_on = [
-    google_project_service.artifact_registry_api
-  ]
-}
-
-############################################
-# Cloud Storage bucket for frontend
-############################################
-
-resource "google_storage_bucket" "frontend_bucket" {
-  name     = "${var.project_name}-${var.environment}-frontend-${random_id.suffix.hex}"
+resource "google_storage_bucket" "frontend" {
+  name     = "${var.project_name}-frontend-${random_id.suffix.hex}"
   location = var.region
 
   uniform_bucket_level_access = true
 
   website {
     main_page_suffix = "index.html"
-    not_found_page   = "404.html"
   }
-
-  depends_on = [
-    google_project_service.storage_api
-  ]
 }
 
 resource "google_storage_bucket_iam_member" "frontend_public" {
-  bucket = google_storage_bucket.frontend_bucket.name
+  bucket = google_storage_bucket.frontend.name
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
 
 ############################################
-# Cloud Storage bucket for memory
+# Storage bucket (memory)
 ############################################
 
-resource "google_storage_bucket" "memory_bucket" {
-  name     = "${var.project_name}-${var.environment}-memory-${random_id.suffix.hex}"
+resource "google_storage_bucket" "memory" {
+  name     = "${var.project_name}-memory-${random_id.suffix.hex}"
   location = var.region
 
   uniform_bucket_level_access = true
-
-  depends_on = [
-    google_project_service.storage_api
-  ]
 }
 
 ############################################
-# Service account for Cloud Run
+# Service Account
 ############################################
 
-resource "google_service_account" "cloud_run_sa" {
-  project      = var.project_id
-  account_id   = "${var.project_name}-${var.environment}-run-sa"
-  display_name = "Cloud Run Service Account"
+resource "google_service_account" "function_sa" {
+  account_id   = "${var.project_name}-function-sa"
+  display_name = "Cloud Function Service Account"
 }
 
-############################################
-# IAM permissions for Cloud Run
-############################################
-
-resource "google_project_iam_member" "run_storage_access" {
+resource "google_project_iam_member" "storage_access" {
   project = var.project_id
   role    = "roles/storage.objectAdmin"
-  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+  member  = "serviceAccount:${google_service_account.function_sa.email}"
+}
+
+resource "google_project_iam_member" "vertex_access" {
+  project = var.project_id
+  role    = "roles/aiplatform.user"
+  member  = "serviceAccount:${google_service_account.function_sa.email}"
 }
 
 ############################################
-# Cloud Run service (backend API)
+# Zip backend code
 ############################################
 
-resource "google_cloud_run_service" "backend" {
-  name     = "${var.project_name}-${var.environment}-backend"
-  location = var.region
+data "archive_file" "function_zip" {
+  type        = "zip"
+  source_dir  = "../backend"
+  output_path = "../backend/function.zip"
+}
 
-  template {
+############################################
+# Upload function code
+############################################
 
-    metadata {
-      annotations = {
-        "run.googleapis.com/client-name" = "terraform"
-      }
-    }
+resource "google_storage_bucket_object" "function_archive" {
+  name   = "function.zip"
+  bucket = google_storage_bucket.memory.name
+  source = data.archive_file.function_zip.output_path
+}
 
-    spec {
-      service_account_name = google_service_account.cloud_run_sa.email
+############################################
+# Cloud Function
+############################################
 
-      containers {
+resource "google_cloudfunctions2_function" "backend" {
+  name        = "${var.project_name}-backend"
+  location    = var.region
+  description = "Digital Twin API"
 
-        image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.backend_repo.repository_id}/twin-backend:latest"
+  build_config {
+    runtime     = "python311"
+    entry_point = "chat"
 
-        ports {
-          container_port = 8000
-        }
-
-        env {
-          name  = "OPENAI_API_KEY"
-          value = var.openai_api_key
-        }
-
-        env {
-          name  = "CORS_ORIGINS"
-          value = "*"
-        }
-
-        env {
-          name  = "MEMORY_BUCKET"
-          value = google_storage_bucket.memory_bucket.name
-        }
+    source {
+      storage_source {
+        bucket = google_storage_bucket.memory.name
+        object = google_storage_bucket_object.function_archive.name
       }
     }
   }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
+  service_config {
+    max_instance_count = 1
+    available_memory   = "512M"
+
+    environment_variables = {
+      MEMORY_BUCKET = google_storage_bucket.memory.name
+      GCP_PROJECT   = var.project_id
+      GCP_REGION    = var.region
+    }
+
+    service_account_email = google_service_account.function_sa.email
   }
 
-  depends_on = [
-    google_project_service.run_api,
-    google_artifact_registry_repository.backend_repo
-  ]
+  depends_on = [google_project_service.services]
 }
 
 ############################################
-# Make Cloud Run public
+# API Gateway
 ############################################
 
-resource "google_cloud_run_service_iam_member" "public_access" {
-  service  = google_cloud_run_service.backend.name
-  location = google_cloud_run_service.backend.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+resource "google_api_gateway_api" "api" {
+  api_id = "${var.project_name}-api"
+}
+
+resource "google_api_gateway_api_config" "api_config" {
+  api      = google_api_gateway_api.api.api_id
+  api_config_id = "v1"
+
+  openapi_documents {
+    document {
+      path = "openapi.yaml"
+      contents = base64encode(file("${path.module}/openapi.yaml"))
+    }
+  }
+}
+
+resource "google_api_gateway_gateway" "gateway" {
+  api_config = google_api_gateway_api_config.api_config.id
+  gateway_id = "${var.project_name}-gateway"
+  location   = var.region
 }
 
 ############################################
 # Outputs
 ############################################
 
-output "backend_url" {
-  value = google_cloud_run_service.backend.status[0].url
+output "api_url" {
+  value = google_api_gateway_gateway.gateway.default_hostname
 }
 
 output "frontend_bucket" {
-  value = google_storage_bucket.frontend_bucket.name
-}
-
-output "memory_bucket" {
-  value = google_storage_bucket.memory_bucket.name
-}
-
-output "artifact_registry_repo" {
-  value = google_artifact_registry_repository.backend_repo.repository_id
+  value = google_storage_bucket.frontend.name
 }
